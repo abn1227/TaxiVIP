2aa865424df6b5a1961ef8c0214042dfe160fd80
PaolaIzaguirre <eviizaguirre@yahoo.com>
2020-05-25 12:28:09 -0600

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-       // $schedule->command('mileage:taxi_drivers')->dailyAt('10:30');
-       $schedule->command('mileage:taxi_drivers')->everyTenMinutes();
+            $schedule->command('mileage:taxi_drivers')->dailyAt('10:30');
     }
 
 
