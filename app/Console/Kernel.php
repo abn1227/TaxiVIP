d4bce37a6220c55b62c7df5a7d868fa4c29c4394
PaolaIzaguirre <eviizaguirre@yahoo.com>
2020-05-25 10:27:45 -0600

    job prueba

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('mileage:taxi_drivers')->dailyAt('09:11');
+        $schedule->command('mileage:taxi_drivers')->dailyAt('10:30');
     }
 
     /**
